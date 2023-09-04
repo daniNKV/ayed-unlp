cea290bbed37f839ee266ede137970baf25bbf6b
Daniel Nikiforov <daninkv@Daniels-MacBook-Air.local>
2023-09-04 17:14:14 -0300

    Prepara practica 1

---
+++ src/main/java/unlp/ar/ayed/practica2/ejercicio1/TestListaDeEnterosConArreglos.java
@@ -0,0 +1,5 @@
+package unlp.ar.ayed.practica2.ejercicio1;
+import unlp.ar.ayed.ListasDeEnteros.bin.tp02.ejercicio1.*;
+public class TestListaDeEnterosConArreglos {
+
+}
---
+++ src/main/java/unlp/ar/ayed/practica2/ejercicio1/TestListaDeEnterosEnlazada.java
@@ -0,0 +1,3 @@
+package unlp.ar.ayed.practica2.ejercicio1;
+public class TestListaDeEnterosEnlazada {
+}
