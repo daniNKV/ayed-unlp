e6cef948a1260751c5773fb86d8f5ef7d74ec67b
Daniel Nikiforov <daninkv@Daniels-MacBook-Air.local>
2023-09-04 20:28:37 -0300

    Completa ejercicios 1 y 2

--- src/main/java/unlp/ar/ayed/practica2/ejercicio1/TestListaDeEnterosConArreglos.java
+++ src/main/java/unlp/ar/ayed/practica2/ejercicio1/TestListaDeEnterosConArreglos.java
@@ -1,5 +1,20 @@
 package unlp.ar.ayed.practica2.ejercicio1;
-import unlp.ar.ayed.ListasDeEnteros.bin.tp02.ejercicio1.*;
-public class TestListaDeEnterosConArreglos {
 
+import lib.ListasDeEnteros.src.tp02.ejercicio1.ListaDeEnterosConArreglos;
+
+public class TestListaDeEnterosConArreglos {
+    public static void main(String [] args) {
+        ListaDeEnterosConArreglos lista = new ListaDeEnterosConArreglos();
+        lista.agregarInicio(1);
+        lista.agregarInicio(2);
+        lista.agregarInicio(3);
+
+        lista.agregarFinal(5);
+
+        lista.comenzar();
+
+        while (!lista.fin()) {
+            System.out.print(lista.proximo() + " ");
+        }
+    }
 }
--- src/main/java/unlp/ar/ayed/practica2/ejercicio1/TestListaDeEnterosEnlazada.java
+++ src/main/java/unlp/ar/ayed/practica2/ejercicio1/TestListaDeEnterosEnlazada.java
@@ -1,3 +1,19 @@
 package unlp.ar.ayed.practica2.ejercicio1;
+
+import lib.ListasDeEnteros.src.tp02.ejercicio1.ListaDeEnterosEnlazada;
+
 public class TestListaDeEnterosEnlazada {
+    public static void main(String [] args) {
+        ListaDeEnterosEnlazada lista = new ListaDeEnterosEnlazada();
+
+        lista.agregarInicio(2);
+        lista.agregarInicio(1);
+        lista.agregarFinal(3);
+
+        lista.comenzar();
+
+        while (!lista.fin()) {
+            System.out.print(lista.proximo() + " ");
+        }
+    }
 }
